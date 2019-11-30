6b7e1f2c5b088684b50ce83eeb53db35d0e56fa6
weto <weto.jc@gmail.com>
2019-11-30 15:03:29 +0000

    Alterando ordem de execucao de script

--- script/start.sh
+++ script/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+proxy=`ufw disable`
 iniciandoDisco=`systemctl daemon-reload`
 desabilitandoSwap=`swapoff -a`
 iniciandoK8s=`systemctl restart kubelet`
-proxy=`ufw disable`
 echo "$proxy && $iniciandoDisco && $desabilitandoSwap && $iniciandoK8s"
 echo "Inicializando Serviços"
