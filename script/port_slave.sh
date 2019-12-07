25019732a98a2f72dc285cf4b2c3a1616d8df74a
weto <weto.jc@gmail.com>
2019-12-07 12:46:58 -0300

    Update port_slave.sh

--- script/port_slave.sh
+++ script/port_slave.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-firewall=`ufw enable -y`
-echo "$firewall"
+#firewall=`ufw enable -y`
+#echo "$firewall"
 
 for i  in  32767 6784 80 8080 22 10255 6783 10250 30000; do
-    echo `ufw allow $i`
+    echo `ufw allow to any port $i`
 done
 
