0944651e6e3a3201aa9993a0b7c77b3785c8e6be
weto <weto.jc@gmail.com>
2019-12-07 12:47:33 -0300

    Update port_master.sh

--- script/port_master.sh
+++ script/port_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-firewall=`ufw enable`
-echo "$firewall"
+#firewall=`ufw enable`
+#echo "$firewall"
 
 for i  in  32767 6784 80 8080 22 10255 6783 10250 30000; do
-    echo `ufw allow $i`
+    echo `ufw allow to any port $i`
 done
 
