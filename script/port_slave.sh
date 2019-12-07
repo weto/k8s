127ffad89dea440f2018092555f19fdc4ee67257
weto <weto.jc@gmail.com>
2019-12-07 14:22:12 +0000

    script de portas slave

--- script/port_slave.sh
+++ script/port_slave.sh
@@ -2,7 +2,7 @@
 firewall=`ufw enable`
 echo "$firewall"
 
-for i  in  80 8080 6443 22 10251 10255 10250 2379 2380 10252; do
+for i  in  32767 6784 80 8080 22 10255 6783 10250 30000; do
     echo `ufw allow $i`
 done
 
