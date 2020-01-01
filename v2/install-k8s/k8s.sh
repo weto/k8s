427b9990cbf32b9c8a4036b2528a8b175d080e13
weto <weto.jc@gmail.com>
2020-01-01 22:07:02 +0000

    Add cenario de canaly deploy

--- v2/install-k8s/k8s.sh
+++ v2/install-k8s/k8s.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-installk8s=`apt-get install -y kubelet kubeadm kubectl`
-echo "########### Instalando o kubernetes ###########"
-echo "$installK8s"
+#installk8s=`apt-get install -y kubelet kubeadm kubectl`
+#echo "########### Instalando o kubernetes ###########"
+#echo "$installK8s"
 
 downloadImagens=`kubeadm config images pull`
 echo "######### Download de Imagens ##########"
 echo "$downloadImagens"
 
-disableSwap=`swapoff -a`
-echo "############# Desabilitando Swap ##############"
-echo "$disableSwap"
+#disableSwap=`swapoff -a`
+#echo "############# Desabilitando Swap ##############"
+#echo "$disableSwap"
 
-iniciandoK8s=`kubeadm init`
-echo "######### Iniciado o cluster ###########"
-echo "$iniciandoK8s"
+#iniciandoK8s=`kubeadm init`
+#echo "######### Iniciado o cluster ###########"
+#echo "$iniciandoK8s"
 
-criandoDiretorios=`mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config`
-echo "######### Criando diretórios ###########"
-echo "$criandoDiretorios"
+#criandoDiretorios=`mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config`
+#echo "######### Criando diretórios ###########"
+#echo "$criandoDiretorios"
 
-installWaveNet=`kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"`
-echo "######## Intalando POD Network WaveNet ########"
-echo "$installWaveNet"
+#installWaveNet=`kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"`
+#echo "######## Intalando POD Network WaveNet ########"
+#echo "$installWaveNet"
 
--- v2/script/installNodeMaster.sh
+++ v2/script/installNodeMaster.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 #Desabilitando Swap
-swapoff -a && sudo sed -i '/ swap / s/^/#/' /etc/fstab
+#swap_off=`wapoff -a && sudo sed -i '/ swap / s/^/#/' /etc/fstab`
+#echo "$swap_off"
 
 # Iniciando o cluster
 IP_MASTER=$1
-kubeadm init --apiserver-advertise-address=$IP_MASTER --node-name $HOSTNAME --pod-network-cidr=10.244.0.0/16
+#kubeadmInit=`kubeadm init --apiserver-advertise-address=$IP_MASTER --node-name $HOSTNAME --pod-network-cidr=10.244.0.0/16`
+kubeadmInit=`kubeadm init --pod-network-cidr=10.244.0.0/16`
+echo "$kubeadmInit"
 #kubeadm init
 
 # Criando diretórios de Pastas
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+pastaKube=`mkdir -p $HOME/.kube`
+pastaconfig=`cp -i /etc/kubernetes/admin.conf $HOME/.kube/config`
+permissao=`chown $(id -u):$(id -g) $HOME/.kube/config`
+
+echo "$pastakube"
+echo "$pastaconfig"
+echo "$permissao"
 
 # Instalar Pod de comunicacao de rede flannel
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+flannelInstall=`kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml`
+echo "$flannelInstall"
 
